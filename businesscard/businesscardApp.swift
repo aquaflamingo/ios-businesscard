1cb9e8d1843142bfd4eafbc0ea77542a1e4d40da
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-09-02 12:30:28 -0400

    Initial Commit

---
+++ businesscard/businesscardApp.swift
@@ -0,0 +1,17 @@
+//
+//  businesscardApp.swift
+//  businesscard
+//
+//  Created by robertsimoes on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct businesscardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
